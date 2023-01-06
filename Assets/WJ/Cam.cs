be5fbb5398ff8fd1cd7b0ff9c709af2b21cf2564
VRWoojin <115488953+VRWoojin@users.noreply.github.com>
2023-01-06 10:37:29 +0900

    카메라 전환 완료

--- Assets/WJ/Cam.cs
+++ Assets/WJ/Cam.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Cam : MonoBehaviour
 {
+    CinemachineVirtualCamera cam;
+    BoxCollider bc;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.FindGameObjectWithTag("Cam2").GetComponent<CinemachineVirtualCamera>();
+        bc = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            cam.Priority = 10;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            cam.Priority = 1;
+        }
     }
 }
