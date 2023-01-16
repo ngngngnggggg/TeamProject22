14f86b30aaa2f0bf6dc0634ef8745d6e2fa0d8a6
ng <115512139+ngngngnggggg@users.noreply.github.com>
2023-01-16 15:31:28 +0900

    1/16

---
+++ Assets/HW/05.Scripts/HW_DoorOpen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW_DoorOpen : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    
+    
+    // 태그가 Player이면 문이 -90도로 부드럽게 회전한다.
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime);
+            
+            
+        }
+    }
+
+
+
+}
