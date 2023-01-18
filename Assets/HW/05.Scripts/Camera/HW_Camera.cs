1cdaaee05a3e13579ef2c5273d8d1a99cfcd0fbb
ng <115512139+ngngngnggggg@users.noreply.github.com>
2023-01-18 09:37:24 +0900

    1/18

--- Assets/HW/05.Scripts/Camera/HW_Camera.cs
+++ Assets/HW/05.Scripts/Camera/HW_Camera.cs
@@ -1,20 +1,23 @@
-using UnityEngine;
+using System;
 using System.Collections;
- 
+using System.Collections.Generic;
+using UnityEngine;
+
 public class HW_Camera : MonoBehaviour
-{
-    [SerializeField] private Camera cam;
-    //카메라의 색
-
-
-
-
-
+{ public GameObject[] objects;
+   public GameObject[] objects2;
+   
+   
+   
+   void Start()
+   {
+       //objects = GameObject.FindGameObjectsWithTag("Enemy");
+       //objects2 = GameObject.FindGameObjectsWithTag("Enemy2");
+   }
+   
+   
+   
+   
+   
 }
-   
-   
-   
-   
-   
-
 
