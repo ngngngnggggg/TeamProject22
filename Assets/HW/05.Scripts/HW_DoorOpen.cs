f07950f5323c51f8b72b0c505f7359d1304ba9b3
ng <115512139+ngngngnggggg@users.noreply.github.com>
2023-01-26 15:13:50 +0900

    1/26 현우 씬 사운드 완료

--- Assets/HW/05.Scripts/HW_DoorOpen.cs
+++ Assets/HW/05.Scripts/HW_DoorOpen.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class HW_DoorOpen : MonoBehaviour
 {
     [SerializeField] private GameObject door;
-    
-    
+    [SerializeField] private AudioSource doorSound;
+    [SerializeField] private AudioClip clip;
+
+
     // 태그가 Player이면 문이 -90도로 부드럽게 회전한다.
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime);
+            doorSound.PlayOneShot(clip);
+            //3초동안 문이 열린다
+            StartCoroutine(OpenDoor());
             
+        }
+    }
+    
+    IEnumerator OpenDoor()
+    {
+        for (int i = 0; i < 90; i++)
+        {
+            door.transform.Rotate(0, -1, 0);
+            yield return new WaitForSeconds(0.02f);
         }
     }
     
